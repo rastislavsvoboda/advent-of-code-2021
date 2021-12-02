e930295e17f62a02eb039caad83d3a4764eee553
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-12-02 06:28:29 +0100

    day 2

---
+++ aoc_2021_d02.py
@@ -0,0 +1,58 @@
+from datetime import datetime
+
+# pypy3.exe .\save.py 2
+
+start = datetime.now()
+lines = open('2.in').readlines()
+# lines = open('2.in0').readlines()
+
+
+def solve1(lines):
+    h_pos = 0 # horizontal position  
+    depth = 0
+    for line in lines:
+        words = line.strip().split()
+        cmd = words[0]
+        x = int(words[1])
+
+        if cmd == 'down':
+            depth += x
+        elif cmd == 'up':
+            depth -= x
+        elif cmd == 'forward':
+            h_pos += x
+        else:
+            print('error')
+
+    # print(h_pos, depth)
+    return h_pos * depth
+
+
+def solve2(lines):
+    h_pos = 0 # horizontal pos
+    depth = 0
+    aim = 0
+    for line in lines:
+        words = line.strip().split()
+        cmd = words[0]
+        x = int(words[1])
+
+        if cmd == 'down':
+            aim += x
+        elif cmd == 'up':
+            aim -= x
+        elif cmd == 'forward':
+            h_pos += x
+            depth += aim * x
+        else:
+            print('error')
+
+    # print(h_pos, depth)
+    return h_pos * depth
+
+
+print(solve1(lines))  # 1488669
+print(solve2(lines))  # 1176514794
+
+stop = datetime.now()
+print("duration:", stop - start)
