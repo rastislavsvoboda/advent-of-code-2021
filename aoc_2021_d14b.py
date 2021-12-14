bde06ff3a426cefae5eb6234cf7e44cfa4ace1ea
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-12-14 12:18:40 +0100

    day 14 - other - recafored

--- aoc_2021_d14b.py
+++ aoc_2021_d14b.py
@@ -1,6 +1,5 @@
 from datetime import datetime
-from collections import defaultdict, Counter
-from typing import NewType
+from collections import Counter
 
 # pypy3.exe .\save.py 14
 
@@ -9,59 +8,66 @@ lines = open('14.in').readlines()
 # lines = open('14.ex1').readlines()
 
 
+def step1(template, data):
+    new_template = [template[0]]
+    for (a, b) in zip(template, template[1:]):
+        new_template.append(data[a+b])
+        new_template.append(b)
+    return new_template
+
+
 def solve1(lines):
     template = [x for x in lines[0].strip()]
     data = {}
     for line in lines[2:]:
-        line = line.strip()
-        words = line.split(' -> ')
-        assert words[0] not in data
-        data[words[0]] = words[1]
+        x, y = line.strip().split(' -> ')
+        data[x] = y
     # print(data)
 
     for s in range(10):
-        template2 = [template[0]]
-        for (a, b) in zip(template, template[1:]):
-            template2.append(data[a+b])
-            template2.append(b)
-        template = template2
+        template = step1(template, data)
         # print("after ", s+1, template)
 
     C = Counter(template)
     return max(C.values()) - min(C.values())
 
 
+def step2(counter, data):
+    new_counter = Counter()
+    for c in counter:
+        # when XY -> Z
+        # for each XY, there are 2 new pairs: XZ, ZY
+        new_counter[c[0] + data[c]] += counter[c]
+        new_counter[data[c] + c[1]] += counter[c]
+    return new_counter
+
+
 def solve2(lines):
     template = lines[0].strip()
     data = {}
     for line in lines[2:]:
-        line = line.strip()
-        words = line.split(' -> ')
-        assert words[0] not in data
-        data[words[0]] = words[1]
+        x, y = line.strip().split(' -> ')
+        data[x] = y
     # print(data)
 
-    C1 = Counter([template[i:i+2] for i in range(len(template)-1)])
-    # print(C1)
+    # do frequency analysis on pairs
+    C = Counter([template[i:i+2] for i in range(len(template)-1)])
+    # print(C)
 
-    # ! counting idea from Jonathan Paulson
     for s in range(40):
-        C2 = Counter()
-        for c in C1:
-            # when XY -> Z
-            # there are 2 new pairs XZ, ZY
-            C2[c[0] + data[c]] += C1[c]
-            C2[data[c] + c[1]] += C1[c]
-        C1 = C2
-        # print("after ", s+1, C1)
+        C = step2(C, data)
+        # print("after ", s+1, C)
 
-    C = Counter()
-    for c in C1:
-        # for XY pairs, only count first X element
-        C[c[0]] += C1[c]
-    # add 1 for last character
-    C[template[-1]] += 1
-    return max(C.values()) - min(C.values())
+    # count single elements
+    C_single = Counter()
+    for c in C:
+        C_single[c[0]] += C[c]
+        C_single[c[1]] += C[c]
+    # each middle char is counted twice, so add 1 for first and last char, and divide each by 2
+    C_single[template[0]] += 1
+    C_single[template[-1]] += 1
+    counts = [c//2 for c in C_single.values()]
+    return max(counts) - min(counts)
 
 
 print(solve1(lines))  # 2915
