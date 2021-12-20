a16725f27d538182e05a8465388d509702fff479
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-12-20 17:56:07 +0100

    day 19 - refactored, cleaned up, improved performance

--- aoc_2021_d19.py
+++ aoc_2021_d19.py
@@ -29,8 +29,8 @@ def parse_entry(entries):
     return (scanner_id, coords)
 
 
-def transform(p, indx):
-    x, y, z = p
+def transform(point, index):
+    x, y, z = point
 
     trans = [
         (x, y, z),
@@ -64,7 +64,7 @@ def transform(p, indx):
         (-z, -x, y)
     ]
 
-    return trans[indx]
+    return trans[index]
 
 
 def p_to_str(p):
@@ -72,51 +72,20 @@ def p_to_str(p):
     return f'{x},{y},{z}'
 
 
+def compute_differences(points):
+    differences = defaultdict(set)
+    point_combinations = list(combinations(points, 2))
+    for (p1, p2) in point_combinations:
+        diff_str = p_to_str(diff(p1, p2))
+        differences[diff_str].add(p1)
+        differences[diff_str].add(p2)
+    return differences
+
+
 def find_common(points0, points1):
-    # TODO: refactor / optimize
-
-    # differencesX - dictionary to map from difference to 2 points
-    # SXD list od differences
-    # cmbsX - list of combinations of 2 points
-    # k - string representation of the difference (probably not needed and tuple can be used)
-
-    # compute differences between points of scanner a
-    differences0 = defaultdict(set)
-    S0D = []
-    cmbs0 = list(combinations(points0, 2))
-    for cmb in cmbs0:
-        p1, p2 = cmb
-        d = diff(p1, p2)
-        S0D.append(d)
-        k = p_to_str(d)
-        differences0[k].add(p1)
-        differences0[k].add(p2)
-    # print(S0D)
-
-    # compute differences between points of scanner b
-    differences1 = defaultdict(set)
-    S1D = []
-    cmbs1 = list(combinations(points1, 2))
-    for cmb in cmbs1:
-        p1, p2 = cmb
-        d = diff(p1, p2)
-        S1D.append(d)
-        k = p_to_str(d)
-        differences1[k].add(p1)
-        differences1[k].add(p2)
-    # print(S1D)
-
-    s0s = set()
-    for x in S0D:
-        s0s.add(p_to_str(x))
-
-    s1s = set()
-    for x in S1D:
-        s1s.add(p_to_str(x))
-
-    overlapping = s0s.intersection(s1s)
-    # print(overlapping)
-    # print(len(overlapping))
+    differences0 = compute_differences(points0)
+    differences1 = compute_differences(points1)
+    overlapping = set(differences0.keys()) & set(differences1.keys())
 
     # 66 is combinations for 12 points
     if len(overlapping) < 66:
@@ -124,49 +93,46 @@ def find_common(points0, points1):
         return False, [], []
 
     # create set of points that are in both scanners
-    C0 = set()
-    C1 = set()
-    for x in overlapping:
-        C0.update(differences0[x])
-        C1.update(differences1[x])
+    common_points_set0 = set()
+    common_points_set1 = set()
+    for diff_str in overlapping:
+        common_points_set0.update(differences0[diff_str])
+        common_points_set1.update(differences1[diff_str])
+
+    # find the point that is closest to the origin        
 
     # sorted common points lists    
-    common_points0L = list(C0)
-    common_points1L = []
+    common_points_list0 = list(common_points_set0)
+    common_points_list1 = []
 
-    init_diff = p_to_str(diff(common_points0L[0], common_points0L[1]))
-    # print("init dif ", init_diff)
-    cand1 = differences1[init_diff]
-    # print("candidates for first pos:", cand1)
-    for c in cand1:
-        # print("trying first", c)
-        common_points1L = []
-        common_points1L.append(c)
+    init_diff = p_to_str(diff(common_points_list0[0], common_points_list0[1]))
+    candidates = differences1[init_diff]
+    for point_candidate in candidates:
+        common_points_list1 = []
+        common_points_list1.append(point_candidate)
         isOk = True
-        for i in range(1, len(common_points0L)):
-            diff_to_find = p_to_str(diff(common_points0L[i-1], common_points0L[i]))
-            next_cand = differences1[diff_to_find]
-
+        for i in range(1, len(common_points_list0)):
+            diff_to_find = p_to_str(diff(common_points_list0[i-1], common_points_list0[i]))
+            next_candidates = differences1[diff_to_find]
             # assuming there is no same difference between more than 2 points
-            assert len(next_cand) == 2
-
-            item = common_points1L[i-1]
-
+            assert len(next_candidates) == 2
+            prev_point = common_points_list1[i-1]
             # the other point
-            rest = [x for x in next_cand if x != item]
-            # print("rest", rest)
-            if len(rest) != 1:
+            other_points = next_candidates - set([prev_point])
+            if len(other_points) != 1:
                 isOk = False
                 break
-            common_points1L.append(rest[0])
+            # if single left, this is the correct point
+            common_points_list1.append(other_points.pop())
         if not isOk:
             continue
-        if len(common_points1L) == len(C1):
+        if len(common_points_list1) == len(common_points_set1):
             break
 
     # print(common_points1L)
+    assert len(common_points_list1) == len(common_points_set1)
 
-    return True, common_points0L, common_points1L
+    return True, common_points_list0, common_points_list1
 
 
 def manhattan_distance(p1, p2):
@@ -194,16 +160,16 @@ def solve(text):
     # print(N)
 
     # final positions of the scanners
-    FINAL = {}
-    FINAL[0] = (0, 0, 0)
+    scanner_positions = {}
+    scanner_positions[0] = (0, 0, 0)
 
     # good tranformation available for each scanner
-    GOOD = {}
+    scanner_transformations = {}
     for i in range(N):
-        GOOD[i] = [x for x in range(24)]
+        scanner_transformations[i] = [x for x in range(24)]
     # scanner 0 is at (0,0,0) so any transf. is the same
     # so assume it is transf. 0 with (x,y,z)
-    GOOD[0] = [0]
+    scanner_transformations[0] = [0]
 
     seen = set()
     Q = deque()
@@ -216,8 +182,8 @@ def solve(text):
         others = [s for s in range(N) if s != s0 and s not in seen]
         for s1 in others:
             sid1, points1 = data[s1]
-            for i in GOOD[s0]:
-                for j in GOOD[s1]:
+            for i in scanner_transformations[s0]:
+                for j in scanner_transformations[s1]:
                     trans_points0 = list(map(lambda p: transform(p, i), points0))
                     trans_points1 = list(map(lambda p: transform(p, j), points1))
                     found, common_points0, common_points1 = find_common(trans_points0, trans_points1)
@@ -235,21 +201,22 @@ def solve(text):
                                 Q.append(s1)
                                 # assuming when found that only one transf. is correct,
                                 # but stored as list for enable iterating
-                                GOOD[s1] = [j]
-                                x1, y1, z1 = FINAL[s0]
+                                scanner_transformations[s1] = [j]
+                                # using prev. scanner position to witch we are comparing to
+                                x1, y1, z1 = scanner_positions[s0]
+                                # and current offset
                                 x2, y2, z2 = single_offset
-                                FINAL[s1] = (x2+x1, y2+y1, z2+z1)
-
-    # print(FINAL)
+                                # compute final position for this scanner
+                                scanner_positions[s1] = (x2+x1, y2+y1, z2+z1)
 
     # all bacons relative to scanner 0 
     all_bacons = set()
     for s, points in data:
         for p in points:
             # transformed bacon point using scanner's transform
-            xb, yb, zb = transform(p, GOOD[s][0])
+            xb, yb, zb = transform(p, scanner_transformations[s][0])
             # scanner offset
-            xs, ys, zs = FINAL[s]
+            xs, ys, zs = scanner_positions[s]
             # point transformed to scanner 0
             x0, y0, z0 = xb + xs, yb + ys, zb + zs
             all_bacons.add((x0, y0, z0))
@@ -260,7 +227,7 @@ def solve(text):
     for s0 in range(N):
         for s1 in range(N):
             if s0 < s1:
-                distances.append(manhattan_distance(FINAL[s0], FINAL[s1]))
+                distances.append(manhattan_distance(scanner_positions[s0], scanner_positions[s1]))
 
     res2 = max(distances)
 
