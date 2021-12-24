1fe3d9ed5f3bdc5b101cd5ea147b0d472dd9fec4
Rastislav Svoboda <rastislav.svoboda@gmail.com>
2021-12-24 16:25:44 +0100

    day 24 - program to check the result

--- aoc_2021_d24.py
+++ aoc_2021_d24.py
@@ -7,6 +7,9 @@ start = datetime.now()
 lines = open('24.in').readlines()
 
 
+# program is basically 14 blocks of the "same" instructions
+# varying on rows:
+
 # row 6: like add x 13 or add x -5
 X = [13, 11, 15, -11, 14, 0, 12, 12, 14, -6, -10, -12, -3, -5]
 
@@ -16,10 +19,14 @@ Y = [13, 10, 5, 14, 5, 15, 4, 11, 1, 15, 12, 8, 14, 9]
 # row 5: div z 1 or div z 26
 Z = [1, 1, 1, 26, 1, 26, 1, 1, 1, 26, 26, 26, 26, 26]
 
-# there are 7 values where Z is 26 and where 1
-
+# only z reg is relevant between "independent" blocks
+# out of 14 values there are 7 values where Z is 26 and where 1
+# "relevant" are these where 26
+# we don't need to brute force 9**14 which is impossible
+# but only 9**7
 
 def is_valid(digits):
+    # reduced algorithm
     z = 0
     res = [0] * 14
 
@@ -44,7 +51,47 @@ def is_valid(digits):
     return res
 
 
-def solve(part):
+def run(program, input):
+    # original program interpretter
+    regs = {'w': 0, 'x': 0, 'y': 0, 'z': 0}
+    pc = 0
+    input_idx = 0
+
+    while pc < len(program):
+        if len(program[pc]) == 2:
+            cmd, a = program[pc]
+            assert cmd == "inp"
+            assert input_idx < len(input)
+            regs[a] = input[input_idx]
+            input_idx += 1
+        else:
+            assert len(program[pc]) == 3
+            cmd, a, reg_or_int = program[pc]
+            b = regs[reg_or_int] if reg_or_int in regs else int(reg_or_int)
+
+            if cmd == "add":
+                regs[a] += b
+            elif cmd == "mul":
+                regs[a] *= b
+            elif cmd == "div":
+                assert b != 0
+                regs[a] //= b
+            elif cmd == "mod":
+                assert regs[a] >= 0
+                assert b > 0
+                regs[a] %= b
+            elif cmd == "eql":
+                regs[a] = 1 if regs[a] == b else 0
+            else:
+                assert False
+        pc += 1
+
+    return regs['z'] == 0
+
+
+def solve(lines, part):
+    instructions = [line.strip().split() for line in lines]
+
     if part == 1:
         # decreasing from 9999999
         input_space = product([9, 8, 7, 6, 5, 4, 3, 2, 1], repeat=7)
@@ -57,12 +104,15 @@ def solve(part):
         if res:
             break
 
+    assert run(instructions, res)
+
     code = "".join([str(i) for i in res])
+
     return code
 
 
-print(solve(1))  # 12934998949199
-print(solve(2))  # 11711691612189
+print(solve(lines, 1))  # 12934998949199
+print(solve(lines, 2))  # 11711691612189
 
 stop = datetime.now()
 print("duration:", stop - start)
